ddf3235db9f3c37aea548e7d09cea993f6e78d6a
Dipankar Shakya <dipankarshakya@lftechnology,com>
2025-07-28 19:55:54 +0545

    testing frontend deployment

--- scripts/deploy-backend.sh
+++ scripts/deploy-backend.sh
@@ -16,4 +16,5 @@ aws ssm send-command \
     --document-name "AWS-RunShellScript" \
     --targets "[{\"Key\":\"InstanceIds\",\"Values\":[\"$EC2_INSTANCE_ID\"]}]" \
     --parameters "{\"commands\":[\"sudo su - root -c '/root/deployment/deployment_script_team2-dipankar.sh'\"]}" \
+    --instance-ids "i-01a33d573c140f24c" \
     --region "$AWS_REGION"
